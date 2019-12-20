41df109d6793d724f642e54e826af5f669695884
spenceradolph <spenceradolph@ymail.com>
2019-12-19 23:55:11 -0600

    added subdomains

---
+++ adolph.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Adolph Sites</title>
+</head>
+<body>
+    <h1>Hey Everyone!</h1>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,8 +1,13 @@
-<html>
-<head>
-    <title>Adolph's Website!</title>
-</head>
-<body>
-    <h1>Testing the new site.</h1>
-</body>
-</html>
+<?php 
+    if ($_SERVER['HTTP_HOST'] === 'gene.adolph.io' || $_SERVER['HTTP_HOST'] === 'www.gene.adolph.io') {
+        include('./gene.php');
+    }elseif ($_SERVER['HTTP_HOST'] === 'julie.adolph.io' || $_SERVER['HTTP_HOST'] === 'www.julie.adolph.io') {
+        include('./julie.php');
+    }elseif ($_SERVER['HTTP_HOST'] === 'regan.adolph.io' || $_SERVER['HTTP_HOST'] === 'www.regan.adolph.io') {
+        include('./regan.php');
+    }elseif ($_SERVER['HTTP_HOST'] === 'wesley.adolph.io' || $_SERVER['HTTP_HOST'] === 'www.wesley.adolph.io') {
+        include('./wesley.php');
+    }else {
+        include('./adolph.php');
+    }
+?>

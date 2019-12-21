fe69b19d6699a629ce77e14a22fc8fcae49d80dc
spenceradolph <spenceradolph@ymail.com>
2019-12-21 16:17:35 -0600

    regan's site

--- regan.php
+++ regan.php
@@ -1,8 +1,76 @@
+<!DOCTYPE html>
+<!--
+	Ethereal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
-<head>
-    <title>Regan Adolph</title>
-</head>
-<body>
-    <h1>Hey Regan!</h1>
-</body>
+  <head>
+    <title>Ethereal by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, user-scalable=no"
+    />
+    <link rel="stylesheet" href="assets_regan/css/main.css" />
+    <noscript
+      ><link rel="stylesheet" href="assets_regan/css/noscript.css"
+    /></noscript>
+  </head>
+  <body class="is-preload">
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+      <!-- Wrapper -->
+      <div id="wrapper">
+        <!-- Panel (Banner) -->
+        <section class="panel banner right">
+          <div class="content color0 span-3-75">
+            <h1 class="major">Regan Adolph<br /></h1>
+            <p>Daughter, Sister, Girlfriend.</p>
+            <ul class="actions">
+              <li>
+                <a
+                  href="#first"
+                  class="button primary color1 circle icon solid fa-angle-right"
+                  >Next</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="image filtered span-1-75" data-position="25% 25%">
+            <img src="images_regan/pic01.jpg" alt="" />
+          </div>
+        </section>
+
+        <!-- Panel (Spotlight) -->
+        <section class="panel spotlight medium right" id="first">
+          <div class="content span-7">
+            <h2 class="major">A little about me.</h2>
+            <p>I like history, tennis, and my family.</p>
+          </div>
+          <div class="image filtered tinted" data-position="top left">
+            <img src="images_regan/pic02.jpg" alt="" />
+          </div>
+        </section>
+
+        <!-- Panel -->
+        <section class="panel">
+          <div class="gallery">
+            <a
+              href="images_regan/gallery/fulls/04.jpg"
+              class="image filtered span-2-5"
+              data-position="top"
+              ><img src="images_regan/regan.jpg" alt=""
+            /></a>
+          </div>
+        </section>
+      </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets_regan/js/jquery.min.js"></script>
+    <script src="assets_regan/js/browser.min.js"></script>
+    <script src="assets_regan/js/breakpoints.min.js"></script>
+    <script src="assets_regan/js/main.js"></script>
+  </body>
 </html>

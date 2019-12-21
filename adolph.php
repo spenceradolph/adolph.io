b7d36c52b77b268c8bfd2587d68a3c9b305b7469
spenceradolph <spenceradolph@ymail.com>
2019-12-21 16:57:13 -0600

    adolph page

--- adolph.php
+++ adolph.php
@@ -1,8 +1,73 @@
+<!DOCTYPE html>
+<!--
+	Ethereal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
-<head>
-    <title>Adolph Sites</title>
-</head>
-<body>
-    <h1>Hey Everyone!</h1>
-</body>
+  <head>
+    <title>The Adolph Family</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, user-scalable=no"
+    />
+    <link rel="stylesheet" href="assets_adolph/css/main.css" />
+    <noscript
+      ><link rel="stylesheet" href="assets_adolph/css/noscript.css"
+    /></noscript>
+  </head>
+  <body class="is-preload">
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+      <!-- Wrapper -->
+      <div id="wrapper">
+        <!-- Panel (Banner) -->
+        <section class="panel banner right">
+          <div class="content color0 span-3-75">
+            <h1 class="major">The Adolph Family<br /></h1>
+            <p>We're pretty fucking amazing.</p>
+            <ul class="actions">
+              <li>
+                <a
+                  href="#first"
+                  class="button primary color1 circle icon solid fa-angle-right"
+                  >Next</a
+                >
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <!-- Panel (Spotlight) -->
+        <section class="panel spotlight medium right" id="first">
+          <div class="content span-7">
+            <h2 class="major">A little about us.</h2>
+            <p>Kicking ass and farming sugar cane.</p>
+          </div>
+          <div class="image filtered tinted" data-position="top left">
+            <img src="images_adolph/pic02.jpg" alt="" />
+          </div>
+        </section>
+
+        <!-- Panel -->
+        <section class="panel">
+          <div class="gallery">
+            <a
+              href="images_adolph/gallery/fulls/04.jpg"
+              class="image filtered span-2-5"
+              data-position="top"
+              ><img src="images_adolph/adolph.jpg" alt=""
+            /></a>
+          </div>
+        </section>
+      </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets_adolph/js/jquery.min.js"></script>
+    <script src="assets_adolph/js/browser.min.js"></script>
+    <script src="assets_adolph/js/breakpoints.min.js"></script>
+    <script src="assets_adolph/js/main.js"></script>
+  </body>
 </html>
